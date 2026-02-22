c8a39621ad0e34fd420ea1415163deca30a70473
Yang Liu <summerliu2333@gmail.com>
2026-02-21 18:15:09 -0800

    Read data to array

--- lab12.cpp
+++ lab12.cpp
@@ -5,5 +5,17 @@
 using namespace std;
 
 int main() {
+    array<int, 30> dailySteps;
+    
+    ifstream stepFile("daily_steps.txt");
+    
+    // Read 30 elements from file into the array
+    for (int i = 0; i < 30; i++) {
+        stepFile >> dailySteps[i];
+    }
+    stepFile.close();
+
+    // Verifying
+    cout << "Step count on Day 1: " << dailySteps[0] << endl;
     return 0;
 }

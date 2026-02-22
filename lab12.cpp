1d5342b08ade27701528a1001fb8d30737a33323
Yang Liu <summerliu2333@gmail.com>
2026-02-21 18:31:32 -0800

    Called Arry Member Functions

--- lab12.cpp
+++ lab12.cpp
@@ -15,7 +15,24 @@ int main() {
     }
     stepFile.close();
 
-    // Verifying
-    cout << "Step count on Day 1: " << dailySteps[0] << endl;
+    cout << "--- Monthly Step Count Analysis ---" << endl;
+    
+    cout << "1. Total tracking days: " << dailySteps.size() << endl;
+    
+    cout << "2. Is step data empty? " << (dailySteps.empty() ? "Yes" : "No") << endl;
+    
+    cout << "3. Step count on Day 1: " << dailySteps.front() << endl;
+    
+    cout << "4. Step count on Day 30: " << dailySteps.back() << endl;
+    
+    cout << "5. Step count on Day 10: " << dailySteps.at(9) << endl;
+    
+    cout << "6. Step count on Day 9 (without boundary check): " << dailySteps[8] << endl;
+
+    cout << " All daily step count: " << endl;
+    for (auto it = dailySteps.begin(); it != dailySteps.end(); it++) {
+        cout << "Day " << (it - dailySteps.begin() + 1) << ": " << *it << endl;
+    }
+
     return 0;
 }
